9f2d04a74ceba03c1e374d50e4621d06f04f1f6b
Evan Noronha <hello@evannoronha.com>
2025-03-21 10:17:20 -0700

    Big Changes! Make the website representative of me

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Evan Noronha';
+export const SITE_DESCRIPTION = 'Evan\'s portfolio and personal web page.';
